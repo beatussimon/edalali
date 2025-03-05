e18d30004c8410d9361fe6e2dd87cdd46fe44904
Beatus Simon <129729984+beatussimon@users.noreply.github.com>
2025-03-05 15:14:10 +0300

    Update admin.py

--- edalali/core/admin.py
+++ edalali/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category, Listing, ListingImage, Availability, Booking, Profile
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Listing)
+admin.site.register(ListingImage)
+admin.site.register(Availability)
+admin.site.register(Booking)
+admin.site.register(Profile)
