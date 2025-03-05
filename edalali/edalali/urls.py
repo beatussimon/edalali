d86f45035c1809af322189f7a1ea8a237a14559e
Beatus Simon <129729984+beatussimon@users.noreply.github.com>
2025-03-05 12:47:13 +0000

    update urls

--- edalali/edalali/urls.py
+++ edalali/edalali/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
         path('accounts/', include('allauth.urls')),
             path('', include('core.urls')),
             ]
-
-            if settings.DEBUG:
-                urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
